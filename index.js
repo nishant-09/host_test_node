7488fc291a1a3335dcd4bb1511d197c249c88528
nishant <nishant.tomar@atrox.io>
2024-05-25 09:09:31 +0530

    ADD : IP package

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import ip from 'ip';
 
-const app = express()
-const port = 80
+const app = express();
+const ipAddress = ip.address();
+
+const port = 3030
 
 app.get('/', (req, res) => {
-  res.send('Dummy Express')
+  res.send(`<h1>Dummy Express listing on ${ipAddress}</h1>`)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`Network access via: http://${ipAddress}:${port}`);
 })
